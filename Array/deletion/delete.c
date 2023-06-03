f3987dfc0473157920815f61d20ddc420a618e10
softweredevloper <ssayam200@gmail.com>
2023-06-03 16:42:07 +0530

    delete in c

---
+++ Array/deletion/delete.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void deleteFromStart(int* arr) {
+    int i = 0;
+    while (arr[i] != 0) {
+        arr[i] = arr[i + 1];
+        i++;
+    }
+}
+
+void deleteFromEnd(int* arr) {
+    int i = 0;
+    while (arr[i] != 0) {
+        i++;
+    }
+    arr[i - 1] = 0;
+}
+
+void deleteAtIndex(int* arr, int index) {
+    int i = index;
+    while (arr[i] != 0) {
+        arr[i] = arr[i + 1];
+        i++;
+    }
+}
+
+void printArr(int* arr) {
+    int i = 0;
+    while (arr[i] != 0) {
+        printf("%d ", arr[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    deleteFromStart(arr);
+    deleteFromEnd(arr);
+    deleteAtIndex(arr, 5);
+    return 0;
+}
