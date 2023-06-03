98ee124d07b73ce2e17477d5ae30be91e2a99ca0
softweredevloper <ssayam200@gmail.com>
2023-06-03 16:34:54 +0530

    inserArray in cpp

---
+++ Array/insertion/insert.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+void insertToStart(int* arr, int key) {
+    int i = 0;
+    while (arr[i] != 0) {
+        i++;
+    }
+    int j = i;
+    while (j >= 0) {
+        arr[j + 1] = arr[j];
+        j--;
+    }
+    arr[0] = key;
+}
+
+void insertToEnd(int* arr, int key) {
+    int i = 0;
+    while (arr[i] != 0) {
+        i++;
+    }
+    arr[i] = key;
+}
+
+void insertAtIndex(int* arr, int index, int key) {
+    int i = 0;
+    while (arr[i] != 0) {
+        i++;
+    }
+    int j = i;
+    while (j >= index) {
+        arr[j + 1] = arr[j];
+        j--;
+    }
+    arr[index] = key;
+}
+
+void printArr(int* arr) {
+    int i = 0;
+    while (arr[i] != 0) {
+        cout << arr[i] << " ";
+        i++;
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    insertToStart(arr, 10);
+    insertToEnd(arr, 30);
+    return 0;
+}
